209587b9d0eed994312acc3eea2af45d7b469a69
el-hadji-cata <paomar8gmail.com>
2025-11-25 11:03:52 +0100

    first commit

---
+++ lib/widgets/searchBar/search_bar_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchBarApp extends StatefulWidget {
+const SearchBarApp({Key? key}) : super(key: key);
+
+@override
+_SearchBarAppState createState() => _SearchBarAppState();
+}
+
+class _SearchBarAppState extends State<SearchBarApp> {
+@override
+Widget build(BuildContext context) {
+return Container();
+}
+}
